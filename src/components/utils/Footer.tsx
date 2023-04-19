e4bd1dc715cf5db3a64114c21cb68fd6cbd4d67e
JuankGF <juankgonzalezf@gmail.com>
2023-04-19 08:52:06 -0400

    Fix footer styles

--- src/components/utils/Footer.tsx
+++ src/components/utils/Footer.tsx
@@ -9,13 +9,9 @@ import SocialMedias from "./SocialMedias";
 export default function Footer() {
   const { isMobile } = useWindowSize();
   return (
-    <footer className="pt-3 px-5 d-flex align-items-center">
-      <Row className="g-3 w-100">
-        <Col
-          xs={5}
-          md={4}
-          className={`position-relative m-auto ${isMobile ? "order-2" : ""}`}
-        >
+    <footer className="pt-3 px-5 py-2 d-flex align-items-center">
+      <Row className="px-3 w-100 justify-content-center">
+        <Col xs={12} sm="auto" className="my-auto px-5">
           {!isMobile && (
             <Image
               src={logo}
@@ -27,52 +23,42 @@ export default function Footer() {
           )}
           <div className="text-muted nowrap">©EnsembleSound 2023</div>
         </Col>
-
-        <Col className={isMobile ? "order-1" : ""}>
-          <Row className="px-3 w-100">
-            {!isMobile && (
-              <Col as="nav">
-                <h6 className="text-primary">Navigation</h6>
-                <Navbar
-                  linkClassName="text-muted text-sm"
-                  className="footer-nav"
-                />
-              </Col>
-            )}
-            <Col xs={12} md="auto">
-              <h6 className="text-primary">Contact</h6>
-              <p className="text-muted">
-                Address:{" "}
-                <small className="select-all-text">
-                  901 SW 15th TER, Cape Coral FL
-                </small>
-              </p>
-              <p className="text-muted">
-                Phone:{" "}
-                <small className="select-all-text">
-                  <a href="tel:1305-609-6067">+1 305 609 6067</a>
-                </small>
-              </p>
-              <p className="text-muted">
-                Email:{" "}
-                <small className="select-all-text">
-                  <a href="mailto:eddyensemble@gmail.com">
-                    eddyensemble@gmail.com
-                  </a>
-                </small>
-              </p>
-            </Col>
-            <Col xs={12} md="auto">
-              <h6 className="text-primary">Social Media Links</h6>
-              <div
-                className={`d-flex w-100 g-3 align-items-center ${
-                  isMobile ? "justify-content-start" : "justify-content-center"
-                }`}
-              >
-                <SocialMedias />
-              </div>
-            </Col>
-          </Row>
+        {!isMobile && (
+          <Col as="nav" md="auto">
+            <h6 className="text-primary">Navigation</h6>
+            <Navbar linkClassName="text-muted text-sm" className="footer-nav" />
+          </Col>
+        )}
+        <Col xs={12} md="auto">
+          <h6 className="text-primary">Contact</h6>
+          <p className="text-muted text-sm">
+            Address:{" "}
+            <small className="select-all-text">
+              901 SW 15th TER, Cape Coral FL
+            </small>
+          </p>
+          <p className="text-muted text-sm">
+            Phone:{" "}
+            <small className="select-all-text">
+              <a href="tel:1305-609-6067">+1 305 609 6067</a>
+            </small>
+          </p>
+          <p className="text-muted text-sm">
+            Email:{" "}
+            <small className="select-all-text">
+              <a href="mailto:eddyensemble@gmail.com">eddyensemble@gmail.com</a>
+            </small>
+          </p>
+        </Col>
+        <Col xs={12} md="auto">
+          <h6 className="text-primary">Social Media Links</h6>
+          <div
+            className={`d-flex w-100 g-3 align-items-center ${
+              isMobile ? "justify-content-start" : "justify-content-center"
+            }`}
+          >
+            <SocialMedias />
+          </div>
         </Col>
       </Row>
     </footer>
